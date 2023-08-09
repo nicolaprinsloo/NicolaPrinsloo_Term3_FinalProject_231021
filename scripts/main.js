ed74d850c2b5f7645cb10e8f51840071f09c05b1
nicolaprinsloo <231021@virtualwindow.co.za>
2023-08-09 15:08:01 +0200

    cards and array added

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+// ----------------------------------------------------------------
+// Trips Array
+// ----------------------------------------------------------------
+
+
+const arrTrips = [
+    {
+      "name": "Malibu",
+      "price": 2999,
+      "description": "Sail along stunning Malibu coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
+      "image": "Logo.svg",
+      "tripLength": "short",
+    },
+    {
+        "name": "BarbieIsland",
+        "price": 9999,
+        "description": "Discover enchanting shores and create memories on a boat cruise to Barbie Island, where imagination and adventure meet.",
+        "image": "Logo.svg",
+        "tripLength": "long",
+      },
+      {
+        "name": "California",
+        "price": 1999,
+        "description": "Sail along stunning Cali coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
+        "image": "Logo.svg",
+        "tripLength": "short",
+      },
+];
+
+console.log(arrTrips);
