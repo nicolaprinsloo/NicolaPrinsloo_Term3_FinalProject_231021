dd0cac6f0b22065ac45c63b194bde0c27758b7e1
nicolaprinsloo <231021@virtualwindow.co.za>
2023-08-22 11:24:48 +0200

    edited cards

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@
 // Trips Array
 // ----------------------------------------------------------------
 
-
 const arrTrips = [
     {
       "name": "Malibu",
@@ -10,21 +9,48 @@ const arrTrips = [
       "description": "Sail along stunning Malibu coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
       "image": "Logo.svg",
       "tripLength": "short",
+      "tripEdition": "kids",
     },
     {
-        "name": "BarbieIsland",
-        "price": 9999,
-        "description": "Discover enchanting shores and create memories on a boat cruise to Barbie Island, where imagination and adventure meet.",
-        "image": "Logo.svg",
-        "tripLength": "long",
-      },
-      {
-        "name": "California",
-        "price": 1999,
-        "description": "Sail along stunning Cali coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
-        "image": "Logo.svg",
-        "tripLength": "short",
-      },
+      "name": "BarbieIsland",
+      "price": 9999,
+      "description": "Discover enchanting shores and create memories on a boat cruise to Barbie Island, where imagination and adventure meet.",
+      "image": "Logo.svg",
+      "tripLength": "long",
+      "tripEdition": "adults",
+    },
+    {
+      "name": "California",
+      "price": 1999,
+      "description": "Sail along stunning Cali coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
+      "image": "Logo.svg",
+      "tripLength": "short",
+      "tripEdition": "limited",
+    },
+    {
+      "name": "LA",
+      "price": 3999,
+      "description": "Sail along stunning LA coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
+      "image": "Logo.svg",
+      "tripLength": "short",
+      "tripEdition": "kids",
+    },
+    {
+      "name": "Hawaii",
+      "price": 9999,
+      "description": "Discover enchanting shores and create memories on a boat cruise to Barbie Island, where imagination and adventure meet.",
+      "image": "Logo.svg",
+      "tripLength": "long",
+      "tripEdition": "adults",
+    },
+    {
+      "name": "Caribbean",
+      "price": 1999,
+      "description": "Sail along stunning Caribbean coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
+      "image": "Logo.svg",
+      "tripLength": "short",
+      "tripEdition": "limited",
+    },
 ];
 
 console.log(arrTrips);
