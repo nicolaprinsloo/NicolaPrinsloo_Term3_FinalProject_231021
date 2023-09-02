5e57c08f80d4d07680ac91542efde5931890b20a
nicolaprinsloo <231021@virtualwindow.co.za>
2023-09-02 18:48:47 +0200

    Restarted with creating a card, toggle and js

--- scripts/main.js
+++ scripts/main.js
@@ -3,70 +3,49 @@
 // ----------------------------------------------------------------
 
 const arrTrips = [
-    {
-      "name": "Malibu",
-      "price": 2999,
-      "description": "Sail along stunning Malibu coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
-      "image": "Logo.svg",
-      "tripLength": "short",
-      "tripEdition": "kids",
-      "origin":"Malibu",
+    { image: "card img.jpg",
+      name: "Malibu Coastline Cruise",
+      description: "Cruise along the Malibu coastline, soak up the sun, and enjoy breathtaking ocean views on this unforgettable nautical adventure.",
+      date: "21 July 2024",
+      code: "#12345",
+      destination: "Round Trip",
+      duration: "From ... To ...",
+      tc: "additional costs included",
+      price: "R2999,00",
     },
-    {
-      "name": "BarbieIsland",
-      "price": 9999,
-      "description": "Discover enchanting shores and create memories on a boat cruise to Barbie Island, where imagination and adventure meet.",
-      "image": "Logo.svg",
-      "tripLength": "long",
-      "tripEdition": "adults",
-      "origin":"Malibu",
+    { image: "card img.jpg",
+      name: "Malibu Coastline Cruise",
+      description: "Cruise along the Malibu coastline, soak up the sun, and enjoy breathtaking ocean views on this unforgettable nautical adventure.",
+      date: "21 July 2024",
+      code: "#12345",
+      destination: "Round Trip",
+      duration: "From ... To ...",
+      tc: "additional costs included",
+      price: "R2999,00",
     },
-    {
-      "name": "California",
-      "price": 1999,
-      "description": "Sail along stunning Cali coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
-      "image": "Logo.svg",
-      "tripLength": "short",
-      "tripEdition": "limited",
-      "origin":"California",
-    },
-    {
-      "name": "Los Angeles",
-      "price": 3999,
-      "description": "Sail along stunning LA coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
-      "image": "Logo.svg",
-      "tripLength": "short",
-      "tripEdition": "kids",
-      "origin":"Los Angeles",
-    },
-    {
-      "name": "Hawaii",
-      "price": 9999,
-      "description": "Discover enchanting shores and create memories on a boat cruise to Barbie Island, where imagination and adventure meet.",
-      "image": "Logo.svg",
-      "tripLength": "long",
-      "tripEdition": "adults",
-      "origin":"Hawaii",
-    },
-    {
-      "name": "Caribbean",
-      "price": 1999,
-      "description": "Sail along stunning Caribbean coast, bask in sunsets, and explore azure waters on a captivating boat cruise.",
-      "image": "Logo.svg",
-      "tripLength": "short",
-      "tripEdition": "limited",
-      "origin":"Caribbean",
+    { image: "card img.jpg",
+      name: "Malibu Coastline Cruise",
+      description: "Cruise along the Malibu coastline, soak up the sun, and enjoy breathtaking ocean views on this unforgettable nautical adventure.",
+      date: "21 July 2024",
+      code: "#12345",
+      destination: "Round Trip",
+      duration: "From ... To ...",
+      tc: "additional costs included",
+      price: "R2999,00",
     },
 ];
 
 let appliedFilter = "";
-let appliedSort = "";
+
 
 console.log(arrTrips);
 
 $(document).ready(function(){
 
-  console.log("Hello");
+  console.log(arrTrips)
+  loadTrips(arrTrips)
+
+  console.log("Hello, I am working");
 
   // -----------------------------------------
   // Trips Page
@@ -79,53 +58,88 @@ $(document).ready(function(){
 // Load all Trips
 // ----------------------------------------------------------------
 
-function loadTrips(tripsToShow) {
+function loadTrips() {
 
   // Load and display all the trips
 
-  console.log(loadTrips);
-  console.log(tripsToShow);
-  
-
-  $.ajax({
-    type:"GET",
-    URL:"https://api.openweathermap.org/data/2.5/weather?q=" + trips.origin + "&appid=6afff1b645e0f71655c4713f2f1c870c",
-    success:function(data){
-      temp = data
-      console.log(temp);
-    }
-  }).done(function(){
-    $(currentChild).find("#weatherTemp").text("OriginTemp: " + Math.round(temp.main.temp - 273));
-  });
+  console.log(arrTrips);
 
   // Clear all elements in container
-  $("#tripsContainer").empty();
+  // $("#tripsContainer").empty();
 
   // Loop through Trips
 
-  for (let i = 0; i < tripsToShow.length; i++) {
-    const trip = tripsToShow[i];
-    
-    console.log(trip);
+  for (let i = 0; i < arrTrips.length; i++) {
+  const trips = arrTrips[i];
 
-    // 1: Select trips container and add the current array trips to it
-    $("#tripsContainer").append($("#tripCardTemplate").html());
+  console.log(trips);
 
-    // 2: Create a variable that contains the most recently added trip card
-    let currentChild = $("#tripsContainer").children().eq(i);
+  $("#tripsContainer").append($("#tripsCardTemplate").html());
 
-    // 3: Set the content for the trip card from the trip list
-    $(currentChild).find(".card-img-top").attr('src','assets/Logo.svg' + trip.image);
-    $(currentChild).find("#nameText").text(trip.name);
-    $(currentChild).find("#priceText").text('R' + trip.price);
-    $(currentChild).find("#descriptionText").text(trip.description);
+  let currentChild = $("#tripsContainer").children().eq(i+1);
 
-    // 4: Hide the description text from the trip card
-    $(currentChild).find("#descriptionText").hide();
-    $(currentChild).find("#weatherTemp").hide();
+  //Show
+  $(currentChild).find(".card-img-top").attr('src', '../assets/' + trips.image);
+  $(currentChild).find("#tripName").text(trips.name);
+  $(currentChild).find("#tripDescription").text(trips.description);
+  $(currentChild).find("#tripDate").text(trips.date); 
+  $(currentChild).find("#tripCode").text(trips.code); 
+  $(currentChild).find("#tripDestination").text(trips.destination); 
+  $(currentChild).find("#tripDuration").text(trips.duration); 
+  $(currentChild).find("#tcInfo").text(trips.tc); 
+  $(currentChild).find("#tripPrice").text(trips.price); 
 
+  // //Hide
+  $(currentChild).find("#tripCode").hide(); 
+  $(currentChild).find("#tripDestination").hide(); 
+  $(currentChild).find("#tripDuration").hide();  
+  $(currentChild).find("#tcInfo").hide(); 
+  $(currentChild).find("#tripPrice").hide(); 
   }
-}
+  
+};
+
+// ----------------------------------------------------------------
+// When the trips card is clicked
+// ----------------------------------------------------------------
+
+$("#tripsContainer").on('click', '.card', function(){
+
+    // Show
+    $(this).find("#tripCode").toggle(); 
+    $(this).find("#tripDestination").toggle(); 
+    $(this).find("#tripDuration").toggle();  
+    $(this).find("#tcInfo").toggle(); 
+    $(this).find("#tripPrice").toggle(); 
+
+    // hide
+    $(this).find("#tripsImage").toggle(); 
+    $(this).find("#tripName").toggle(); 
+    $(this).find("#tripDescription").toggle(); 
+    $(this).find("#tripDate").toggle(); 
+
+});
+    
+    // console.log(trip);
+
+    // // 1: Select trips container and add the current array trips to it
+    // $("#tripsContainer").append($("#tripCardTemplate").html());
+
+    // // 2: Create a variable that contains the most recently added trip card
+    // let currentChild = $("#tripsContainer").children().eq(i);
+
+    // // 3: Set the content for the trip card from the trip list
+    // $(currentChild).find(".card-img-top").attr('src','assets/Logo.svg' + trip.image);
+    // $(currentChild).find("#nameText").text(trip.name);
+    // $(currentChild).find("#priceText").text('R' + trip.price);
+    // $(currentChild).find("#descriptionText").text(trip.description);
+
+    // // 4: Hide the description text from the trip card
+    // $(currentChild).find("#descriptionText").hide();
+    // $(currentChild).find("#weatherTemp").hide();
+
+//   }
+// }
 
 // ----------------------------------------------------------------
 // When a filter or sort is clicked
@@ -162,17 +176,17 @@ function filterSortTrips() {
 // When the trips card is clicked
 // ----------------------------------------------------------------
 
-$("#tripsContainer").on('click', '.card', function(){
+// $("#tripsContainer").on('click', '.card', function(){
 
-  // Toggle the price & description text
-  $(this).find("#priceText").toggle();
-  $(this).find("#descriptionText").toggle();
-  $(this).find("#weatherTemp").toggle();
+//   // Toggle the price & description text
+//   $(this).find("#priceText").toggle();
+//   $(this).find("#descriptionText").toggle();
+//   $(this).find("#weatherTemp").toggle();
 
-  // Resize the image to fit the additional content
-  $(this).find(".card-img-top").toggleClass("small");
+//   // Resize the image to fit the additional content
+//   $(this).find(".card-img-top").toggleClass("small");
 
-});
+// });
 
 //Weather API 
 
